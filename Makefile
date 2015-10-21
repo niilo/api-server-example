1c1059902e710675b2c7f2edc5ada43f3d2eb0dc
Niilo Ursin <niilo.ursin@gmail.com>
2015-10-21 22:51:04 +0300

    swagger documentation.

--- Makefile
+++ Makefile
@@ -4,19 +4,22 @@ get-dev-dependencies:
 vendor:
 	docker run --rm -it -v $(PWD):/app -e "GOPATH=/app/vendor" -w /app golang:1 go get -d
 
-create-swagger-document:
-	swagger -apiPackage="github.com/niilo/api-server-example" -mainApiFile="github.com/niilo/api-server-example/main.go"
+generate-swagger:
+	mkdir -p sw-generate
+	cp *.go sw-generate
+	swagger -apiPackage="github.com/niilo/api-server-example/sw-generate" -mainApiFile="github.com/niilo/api-server-example/sw-generate/server.go"
+	rm -rf sw-generate
 
-build-static-go-binary:
+build-static-binary:
 	docker run --rm -it -v $(PWD):/app -e "GOPATH=/app/vendor" -w /app golang:1 sh -c 'CGO_ENABLED=0 go build -a --installsuffix cgo --ldflags="-s" -o app'
 
-run:
-	docker run --rm -it -v $(PWD):/app -p 8080:8080 iron/base ./app
+run-in-docker:
+	docker run --rm -it -v $(PWD):/app -w /app -p 1323:1323 golang:1 ./app
 
-build-docker-image:
+docker-image-build:
 	docker build -t niilo/api-server-example .
 
-run-docker:
+docker-image-run:
 	docker run --rm -it -p 1323:1323 niilo/api-server-example
 
 ebs-create-zip:
--- server.go
+++ server.go
@@ -1,7 +1,16 @@
+// @APIVersion 1.0.0
+// @BasePath /
+// @APITitle Api Server Example
+// @APIDescription Api Server Example
+// @Contact <email>
+// @TermsOfServiceUrl https://github.com/niilo/api-server-example
+// @License MIT
+// @LicenseUrl https://github.com/niilo/api-server-example/LICENSE.md
 package main
 
 import (
 	"net/http"
+	"strings"
 
 	"github.com/labstack/echo"
 	mw "github.com/labstack/echo/middleware"
@@ -10,14 +19,18 @@ import (
 )
 
 type (
-	user struct {
+	User struct {
 		ID   string `json:"id"`
 		Name string `json:"name"`
+	}
+
+	Users struct {
+		Users map[string]User `json:"users"`
 	}
 )
 
 var (
-	users map[string]user
+	users Users
 )
 
 //----------
@@ -29,20 +42,35 @@ func welcome(c *echo.Context) error {
 }
 
 func createUser(c *echo.Context) error {
-	u := new(user)
+	u := new(User)
 	if err := c.Bind(u); err != nil {
 		return err
 	}
-	users[u.ID] = *u
+	users.Users[u.ID] = *u
 	return c.JSON(http.StatusCreated, u)
 }
 
+// @Title getUsers
+// @Description get all users
+// @Accept  json
+// @Param   id     path    string     true        "User Id"
+// @Success 200 {object} Users
+// @Failure 400 {object} error "users not found"
+// @Router /users [get]
 func getUsers(c *echo.Context) error {
 	return c.JSON(http.StatusOK, users)
 }
 
+// @Title getUser
+// @Description get user by Id
+// @Accept  json
+// @Param   id     path    string     true        "User Id"
+// @Success 200 {object} User
+// @Failure 400 {object} error "id required"
+// @Failure 400 {object} error "user not found for id"
+// @Router /users/{id} [get]
 func getUser(c *echo.Context) error {
-	return c.JSON(http.StatusOK, users[c.P(0)])
+	return c.JSON(http.StatusOK, users.Users[c.P(0)])
 }
 
 func main() {
@@ -108,15 +136,31 @@ func main() {
 		return c.String(http.StatusOK, "Your files!")
 	})
 
+	for apiKey, _ := range apiDescriptionsJson {
+		e.Get("/docs/"+apiKey, ApiDescriptionHandler)
+	}
+
 	// Start server
 	e.Run(":1323")
 }
 
 func init() {
-	users = map[string]user{
-		"1": user{
+	u := Users{}
+	u.Users = map[string]User{
+		"1": User{
 			ID:   "1",
 			Name: "Wreck-It Ralph",
 		},
+	}
+	users = u
+}
+
+func ApiDescriptionHandler(w http.ResponseWriter, r *http.Request) {
+	apiKey := strings.TrimLeft(r.RequestURI, "docs/")
+
+	if json, ok := apiDescriptionsJson[apiKey]; ok {
+		w.Write([]byte(json))
+	} else {
+		w.WriteHeader(http.StatusNotFound)
 	}
 }
